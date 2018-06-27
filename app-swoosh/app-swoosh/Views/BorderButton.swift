50e9b8a174fecbc5152b6b3ab6e84155bda04502
Gurpreet Bhoot <gur_boo15@Gurpreets-MacBook-Pro.local>
2018-06-26 17:07:58 -0700

    added xcode project and initial welcome screen UI

---
+++ app-swoosh/app-swoosh/Views/BorderButton.swift
@@ -0,0 +1,16 @@
+//
+//  BorderButton.swift
+//  app-swoosh     /
+//
+
+import UIKit
+
+class BorderButton: UIButton {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        layer.borderWidth = 3.0
+        layer.borderColor = UIColor.white.cgColor
+    }
+
+}
