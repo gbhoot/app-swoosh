f16a4297e2c9defc03e00e0e75b31a5bec8c13a5
Gurpreet Bhoot <gur_boo15@Gurpreets-MacBook-Pro.local>
2018-06-26 21:14:32 -0700

    made changes with auto layout:

--- app-swoosh/app-swoosh/ViewController.swift
+++ app-swoosh/app-swoosh/ViewController.swift
@@ -18,9 +18,9 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view, typically from a nib.
         
         
-        swoosh.frame = CGRect(x: (view.frame.size.width / 2) - (swoosh.frame.size.width / 2), y: 50, width: swoosh.frame.size.width, height: swoosh.frame.size.height)
-        
-        bgImg.frame = view.frame
+//        swoosh.frame = CGRect(x: (view.frame.size.width / 2) - (swoosh.frame.size.width / 2), y: 50, width: swoosh.frame.size.width, height: swoosh.frame.size.height)
+//        
+//        bgImg.frame = view.frame
     }
 
     override func didReceiveMemoryWarning() {
