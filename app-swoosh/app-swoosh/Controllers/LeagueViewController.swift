3e93e4241949c97c1a84da4d815841cdd202f2f3
Gurpreet Bhoot <gur_boo15@Gurpreets-MacBook-Pro.local>
2018-06-27 20:53:36 -0700

    pass data through view controllers

--- app-swoosh/app-swoosh/Controllers/LeagueViewController.swift
+++ app-swoosh/app-swoosh/Controllers/LeagueViewController.swift
@@ -10,10 +10,18 @@ import UIKit
 
 class LeagueViewController: UIViewController {
     
+    var player: Player!
+    
+    @IBOutlet weak var nextBtn: BorderButton!
+//    @IBOutlet weak var mensBtn: BorderButton!
+//    @IBOutlet weak var womensBtn: BorderButton!
+//    @IBOutlet weak var coedBtn: BorderButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
+        player = Player()
     }
 
     override func didReceiveMemoryWarning() {
@@ -25,8 +33,42 @@ class LeagueViewController: UIViewController {
         performSegue(withIdentifier: "skillVCSegue", sender: self)
     }
     
+    @IBAction func onMensTapped(_ sender: Any) {
+        selectLeague(leagueType: "mens")//, whichBtn: mensBtn)
+    }
+    
+    @IBAction func onWomensTapped(_ sender: Any) {
+        selectLeague(leagueType: "womens")//, whichBtn: womensBtn)
+    }
+    
+    @IBAction func onCoedTapped(_ sender: Any) {
+        selectLeague(leagueType: "coed")//, whichBtn: coedBtn)
+    }
+    
     @IBAction func unwindFromSkillVC(unwindSegue: UIStoryboardSegue) {
         
+    }
+    
+    func selectLeague(leagueType: String) {//}, whichBtn: UIButton) {
+        player.desiredLeague = leagueType
+        nextBtn.isEnabled = true
+//        whichBtn.isSelected = true
+//        if whichBtn == mensBtn {
+//            womensBtn.isSelected = false
+//            coedBtn.isSelected = false
+//        } else if whichBtn == womensBtn {
+//            mensBtn.isSelected = false
+//            coedBtn.isSelected = false
+//        } else {
+//            womensBtn.isSelected = false
+//            mensBtn.isSelected = false
+//        }
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if let skillVC = segue.destination as? SkillViewController {
+            skillVC.player = player
+        }
     }
     
 
---
+++ app-swoosh/app-swoosh/Model/Player.swift
@@ -0,0 +1,14 @@
+//
+//  Player.swift
+//  app-swoosh
+//
+//  Created by Gurpreet Bhoot on 6/27/18.
+//  Copyright © 2018 Gurpal Bhoot. All rights reserved.
+//
+
+import Foundation
+
+struct Player {
+    var desiredLeague: String!
+    var selectedSkillLevel: String!
+}
