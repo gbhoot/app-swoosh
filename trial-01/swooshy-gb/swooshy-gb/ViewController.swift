4fcfb0be20f5987258b1067ca2637297ad910f64
Gurpreet Bhoot <gur_boo15@Gurpreets-MacBook-Pro.local>
2018-07-15 22:34:21 -0700

    began trial - created xcode project

---
+++ trial-01/swooshy-gb/swooshy-gb/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  swooshy-gb
+//
+//  Created by Gurpreet Bhoot on 7/15/18.
+//  Copyright © 2018 Gurpal Bhoot. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
